a70dac74650046397a7a3214954d008f5e2b8b90
Jeremy Desvaux <jeremy@jdmweb.com>
2025-01-07 11:05:10 +0100

    chores(autoloader): avoid deprecation warning if path.root config is not set

--- src/Traits/HasHookAutoloader.php
+++ src/Traits/HasHookAutoloader.php
@@ -7,7 +7,7 @@ trait HasHookAutoloader
     public function autoload(array $classNameFromFiles = [], array $discoveryPaths = [], callable $successCallback = null, array $excludedClasses=[]): array
     {
         $discoveryPathsRoots = $this->manager->getConfig('discoveryPathsRoots', [
-            'hooks' => rtrim($this->manager->getConfig('path.root'), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR
+            'hooks' => rtrim($this->manager->getConfig('path.root') ?? '', DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR
         ]);
         $discoverFolderSuffix = $this->manager->getConfig('cptservice.discoverFolderSuffix', 'Hooks');
         $defaultPaths = $this->deductDefaultDiscoveryPaths($discoveryPathsRoots, $discoverFolderSuffix);
